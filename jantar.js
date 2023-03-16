1eeae727c0311a9b193f03accdc88d4b2ae187c3
Giovanni Filho <giovanni.filho@i-value.int>
2023-03-15 21:06:19 -0300

    arrays-js

---
+++ jantar.js
@@ -0,0 +1,12 @@
+//* métodos e propriedades do javascript *//
+
+jantarDeHoje = ['🍔', '🌭', '🍕'];
+
+jantarDeHoje.pop();
+jantarDeHoje.pop();
+
+jantarDeHoje.push('🍳');
+jantarDeHoje.push('🥗');
+jantarDeHoje.push('🍏');
+
+console.log(`O jantar de hoje será: ${jantarDeHoje}!!! `);
---
+++ media.js
@@ -0,0 +1,19 @@
+/*tirar a média */
+
+/*ao invés de declarar uma const para cada nota como:
+/* const nota1 = 10;
+const nota2 = 6.5;
+const nota3 = 8;
+const nota4 = 7.5;
+ 
+mais dinâmico utilizar o array
+*/
+
+const notas = [10, 6.5, 8, 7.5];
+
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+
+console.log(media);
+
+/* para ser mais escalável length deve ser utilizado pois ele é o número
+maior do que a largura do maior índice*/
